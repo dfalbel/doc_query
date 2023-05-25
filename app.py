3959514f62a7a68084fb632928f41b7eea784e58
Daniel Falbel <dfalbel@gmail.com>
2023-05-26 00:05:31 +0200

    changes to run using a transformers model

--- app.py
+++ app.py
@@ -17,44 +17,24 @@ from chromadb.config import Settings  # pyright: ignore[reportMissingTypeStubs]
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from shiny import App, Inputs, Outputs, Session, reactive, render, ui
 
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
+from threading import Thread
+
 import chatstream
 from chatstream import openai_types
 
-# TODO: Make it so this runs only on connect (not on a local computer)
-torch_cache_dir = Path.home() / ".cache" / "torch"
-if not torch_cache_dir.exists():
-    if not torch_cache_dir.parent.exists():
-        torch_cache_dir.parent.mkdir(parents=True)
 
-    local_torch_cache_dir = Path(__file__).parent / "torch_cache"
-    if not local_torch_cache_dir.exists():
-        local_torch_cache_dir.mkdir()
-
-    print(f"Creating symlink {torch_cache_dir} to {local_torch_cache_dir}")
-    torch_cache_dir.symlink_to(local_torch_cache_dir)
-
+model_id = "stabilityai/stablelm-tuned-alpha-3b"
+#model_id = "EleutherAi/pythia-70m"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(model_id)
 
 # Maximum number of context chunks to send to the API.
 N_DOCUMENTS = 30
 # Maximum number of tokens in the context chunks to send to the API.
-CONTEXT_TOKEN_LIMIT = 3200
+CONTEXT_TOKEN_LIMIT = 200
 # Print debugging info to the console
 DEBUG = True
-
-# Avoid the following warning:
-# huggingface/tokenizers: The current process just got forked, after parallelism has
-# already been used. Disabling parallelism to avoid deadlocks...
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
-# Code for initializing popper.js tooltips.
-tooltip_init_js = """
-var tooltipTriggerList = [].slice.call(
-  document.querySelectorAll('[data-bs-toggle="tooltip"]')
-);
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl);
-});
-"""
 
 app_ui = x.ui.page_fillable(
     ui.head_content(ui.tags.title("Posit Document Query")),
@@ -65,13 +45,6 @@ app_ui = x.ui.page_fillable(
             ui.output_ui("collection_selector_ui"),
             ui.hr(),
             ui.input_select("model", "Model", choices=["gpt-3.5-turbo"]),
-            ui.p(ui.h5("Export conversation")),
-            ui.input_radio_buttons(
-                "download_format", None, ["Markdown", "JSON"], inline=True
-            ),
-            ui.div(
-                ui.download_button("download_conversation", "Download"),
-            ),
             ui.hr(),
             ui.p(
                 "Built with ",
@@ -87,10 +60,15 @@ app_ui = x.ui.page_fillable(
             width=280,
             position="right",
         ),
-        chatstream.chat_ui("chat1"),
+        ui.div(
+            ui.output_ui("messages_ui"),
+            x.ui.layout_column_wrap(
+                0.5,
+                ui.input_text("query", "", width="100%"),
+                ui.input_action_button("send", "Send", width="100%"),
+            )
+        )
     ),
-    # Initialize the tooltips at the bottom of the page (after the content is in the DOM)
-    ui.tags.script(tooltip_init_js),
 )
 
 # ======================================================================================
@@ -100,7 +78,6 @@ chroma_client = chromadb.Client(
         persist_directory=str((Path(__file__).parent / "doc_db").resolve()),
     )
 )
-
 
 def server(input: Inputs, output: Outputs, session: Session):
     # collection: reactive.Value[Collection] = reactive.Value()
@@ -126,6 +103,57 @@ def server(input: Inputs, output: Outputs, session: Session):
         return chroma_client.get_collection(  # pyright: ignore[reportUnknownMemberType]
             input.collection()
         )
+    
+    messages = reactive.Value([])
+    generating = reactive.Value(False)
+
+    @reactive.Effect
+    @reactive.event(input.send)
+    def _():
+        updated = messages() + [{'role': 'user', 'content': input.query()}]
+        messages.set(updated)
+        generating.set(True)
+
+    @output(priority=10)
+    @render.ui
+    def messages_ui():
+        print("rendering messages")
+        return ui_messages(messages())
+    
+    streamer = reactive.Value()
+    count_tokens = reactive.Value(0)
+
+    @reactive.Effect
+    @reactive.event(generating)
+    def _():
+        if not generating():
+            return
+        query = add_context_to_query(input.query())
+        inputs = tokenizer(query, return_tensors="pt")
+        
+        stream = TextIteratorStreamer(tokenizer, skip_prompt=True)
+        generation_kwargs = dict(**inputs, streamer=stream, max_new_tokens=30)
+        
+        thread = Thread(target=model.generate, kwargs=generation_kwargs)
+        thread.start()
+
+        streamer.set(stream)
+        count_tokens.set(count_tokens()+1)
+        messages.set(messages() + [{'role': 'bot', 'content': ''}])
+
+    @reactive.Effect
+    @reactive.event(count_tokens)
+    def _():
+        print("getting next token")
+        try:
+            token = next(streamer())
+        except StopIteration:
+            generating.set(False)
+            return
+        msgs = messages().copy()
+        msgs[-1]['content'] += token
+        messages.set(msgs)
+        count_tokens.set(count_tokens()+1)
 
     def add_context_to_query(query: str) -> str:
         results = collection().query(
@@ -163,31 +191,6 @@ def server(input: Inputs, output: Outputs, session: Session):
 
         return prompt_template
 
-    chat_session = chatstream.chat_server(
-        "chat1",
-        model=input.model,
-        query_preprocessor=add_context_to_query,
-        debug=True,
-    )
-
-    def download_conversation_filename() -> str:
-        if input.download_format() == "JSON":
-            ext = "json"
-        else:
-            ext = "md"
-        return f"conversation-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.{ext}"
-
-    @session.download(filename=download_conversation_filename)
-    def download_conversation() -> Generator[str, None, None]:
-        if input.download_format() == "JSON":
-            res = chatstream.chat_messages_enriched_to_chat_messages(
-                chat_session.session_messages()
-            )
-            yield json.dumps(res, indent=2)
-
-        else:
-            yield chat_messages_to_md(chat_session.session_messages())
-
 
 app = App(app_ui, server)
 
@@ -196,77 +199,16 @@ app = App(app_ui, server)
 # Utility functions
 # ======================================================================================
 
-
-def chat_messages_to_md(messages: Sequence[openai_types.ChatMessage]) -> str:
-    """
-    Convert a list of ChatMessage objects to a Markdown string.
-
-    Parameters
-    ----------
-    messages
-        A list of ChatMessageobjects.
-
-    Returns
-    -------
-    str
-        A Markdown string representing the conversation.
-    """
-    res = ""
-
-    for message in messages:
-        if message["role"] == "system":
-            # Don't show system messages.
-            continue
-
-        res += f"## {message['role'].capitalize()}\n\n"
-        res += message["content"]
-        res += "\n\n"
-
-    return res
-
-
-def extract_text_from_pdf(pdf_path: str | Path) -> str:
-    with open(pdf_path, "rb") as file:
-        pdf_reader = pypdf.PdfReader(file)
-
-        lines: list[str] = []
-        for i in range(len(pdf_reader.pages)):
-            lines.append(pdf_reader.pages[i].extract_text())
-
-    return "\n".join(lines)
-
-
-async def add_file_content_to_db(
-    collection: chromadb.api.Collection,
-    file: str | Path,
-    label: str,
-    debug: bool = False,
-) -> None:
-    file = Path(file)
-
-    with ui.Progress(min=1, max=15) as p:
-        p.set(message="Extracting text...")
-        await asyncio.sleep(0)
-        if file.suffix.lower() == ".pdf":
-            text = extract_text_from_pdf(file)
-        else:
-            text = file.read_text()
-
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-        text_chunks = text_splitter.split_text(text)
-
-    if debug:
-        print(json.dumps(text_chunks, indent=2))
-
-    with ui.Progress(min=1, max=len(text_chunks)) as p:
-        for i in range(len(text_chunks)):
-            p.set(value=i, message="Adding text to database...")
-            await asyncio.sleep(0)
-            collection.add(
-                documents=text_chunks[i],
-                metadatas={"filename": label, "page": str(i)},
-                ids=f"{label}-{i}",
-            )
+def ui_messages (messages):
+    return ui.div(*[
+        x.ui.card(
+            {'style': "margin-bottom:5px;"},
+            x.ui.card_body(
+                ui.p(message['content'])
+            ),
+        )
+        for message in messages]
+    )
 
 
 def get_token_count(s: str, model: str) -> int:
